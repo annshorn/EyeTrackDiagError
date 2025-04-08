bde7c46e71a010b98bd412d8bf7d6b2f88c0a0dc
Anna <49962972+annshorn@users.noreply.github.com>
2025-04-08 12:55:31 +0200

    Update dataloader.py

--- lib/dataloader.py
+++ lib/dataloader.py
@@ -4,62 +4,58 @@ from .utils import load_pickle_file, save_dicts
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 
-def create_dict_dataset(df, data_folder, feature_folder='gru', feature_name_csv='gaze_features.csv'):
-    """Reads gaze feature CSV files from a structured dataset and creates a dictionary 
-    of DataFrames grouped by 'index_row'.
-    
-    Args:
-        df (pd.DataFrame): Input DataFrame containing metadata.
-        data_folder (str): Base path to the dataset.
-        feature_folder (str, optional): Subfolder where features are stored. Default: 'gru'.
-        feature_name_csv (str, optional): Feature filename. Default: 'gaze_features.csv'.
-    
-    Returns:
-        dict: Dictionary of DataFrames grouped by 'index_row'.
-    """
+# Please split your data and prepare DataFrames: ROOT_PATH/train.csv, ROOT_PATH/test.csv, and ROOT_PATH/val.csv
+ROOT_PATH = '/home/csn801/__allData/locked_gaze_data'
+PATH_TO_SAVEDATA = '/home/csn801/Elsevier25/data'
+FOLDER_NAME = 'elsevier25'
+FEATURES_NAME = 'features.csv'
+EMB_NAME = 'composite_embeddings_square.pt'
+
+
+def create_dict_dataset(df):
+    """Takes gaze feature data and embeddings from a structured dataset and creates a dictionary 
+    of DataFrames grouped by 'index_row'."""
     index_row = 0
     final_df = pd.DataFrame()
     for index, row in df.iterrows():
-        path_to_features = os.path.join(data_folder, str(int(row['case'])), 
-                                        row['gaze_path'], feature_folder, feature_name_csv)
-        if os.path.exists(path_to_features):
+        if str(int(row['case'])) == '600':
+            path = os.path.join(ROOT_PATH, row['gaze_path'][:-13], FOLDER_NAME)
+        else:
+            path = os.path.join(ROOT_PATH, str(int(row['case'])), row['gaze_path'][:-8], FOLDER_NAME)
+        path_to_features = os.path.join(path, FEATURES_NAME)
+        path_to_emb = os.path.join(path, EMB_NAME)
+        if os.path.exists(path_to_features) and os.path.exists(path_to_emb):
             current_feature_df = pd.read_csv(path_to_features)
             if 'Unnamed: 0' in current_feature_df.columns:
-                current_feature_df = current_feature_df.drop(['Unnamed: 0'], axis=1)
-            
-            current_feature_df['radiologist'] = row.get('radiologist', None)
+                merged_df = current_feature_df.drop(['Unnamed: 0'], axis=1)
+            current_emb = torch.load(path_to_emb)
 
-            current_feature_df['label'] = row['label']
-            current_feature_df['index_row'] = index_row
+            assert current_emb.shape[0] == current_feature_df.shape[0], f"Shape mismatch: {current_emb.shape} vs {current_feature_df.shape}"
+            
+            current_emb_df = pd.DataFrame(current_emb.numpy(), 
+                                columns=[f'emb_{i}' for i in range(current_emb.shape[1])])
+            merged_df = pd.concat([current_feature_df.reset_index(drop=True), current_emb_df], axis=1)
+            merged_df['index_row'] = index_row
+            final_df = pd.concat([final_df, merged_df], ignore_index=True)
             index_row+=1
-            final_df = pd.concat([final_df, current_feature_df], ignore_index=True)
+        if os.path.exists(path_to_features) and not os.path.exists(path_to_emb):
+            print(f"Path to features exists but not to embeddings: {path_to_features}")
+        if not os.path.exists(path_to_features):
+            print(f"Path to features does not exist: {path_to_features}")
     final_df = dict(list(final_df.groupby(['index_row'])))
     return final_df
 
-def prepare_longitudinal_dataset(data_folder):
-    """Prepares and processes longitudinal datasets.
+def prepare_longitudinal_dataset():
+    splitted_train_df = pd.read_csv(os.path.join(PATH_TO_SAVEDATA, "train.csv"))
+    splitted_test_df = pd.read_csv(os.path.join(PATH_TO_SAVEDATA, "test.csv"))
+    splitted_val_df = pd.read_csv(os.path.join(PATH_TO_SAVEDATA, "val.csv"))
 
-    Args:
-        data_folder (str): Path to the folder containing the dataset CSV files.
-
-    Returns:
-        tuple: A tuple containing three dictionaries:
-            - train (dict): Processed training dataset.
-            - test (dict): Processed test dataset.
-            - val (dict): Processed validation dataset.
-    """
-    splitted_train_df = pd.read_csv(os.path.join(data_folder, "train.csv"))
-    splitted_test_df = pd.read_csv(os.path.join(data_folder, "test.csv"))
-    splitted_val_df = pd.read_csv(os.path.join(data_folder, "val.csv"))
-
-    train = create_dict_dataset(splitted_train_df, data_folder)
-    test = create_dict_dataset(splitted_test_df, data_folder)
-    val = create_dict_dataset(splitted_val_df, data_folder)
-
-    save_dicts(train, os.path.join(data_folder, "grouped_train"))
-    save_dicts(test, os.path.join(data_folder, "grouped_test"))
-    save_dicts(val, os.path.join(data_folder, "grouped_val"))
-
+    train = create_dict_dataset(splitted_train_df)
+    test = create_dict_dataset(splitted_test_df)
+    val = create_dict_dataset(splitted_val_df)
+    save_dicts(train, os.path.join(PATH_TO_SAVEDATA, "grouped_elsevier25_train"))
+    save_dicts(test, os.path.join(PATH_TO_SAVEDATA, "grouped_elsevier25_test"))
+    save_dicts(val, os.path.join(PATH_TO_SAVEDATA, "grouped_elsevier25_val"))
     return train, test, val
 
 def return_reading_seq(seq, sequence_length):
@@ -80,8 +76,8 @@ def return_reading_seq(seq, sequence_length):
 def prepare_features(df):
     """Prepares and cleans the features from a pandas DataFrame for model input."""
     columns_to_drop = [
-        'win_w', 'win_h', 'radiologist',
-        'case', 'fname', 'gaze_path',
+        'win_w', 'win_h', 'rad_name',#'win_width', 'win_height', 'radiologist',
+        'case', 'fname',
         'label'
     ]
     df = df.drop(columns_to_drop, axis=1)
@@ -107,57 +103,35 @@ def prepare_features(df):
         df = df.drop(['doctor_confidence'], axis=1)
     if 'index_row' in  df.columns:
         df = df.drop(['index_row'], axis=1)
+    if 'gaze_path' in  df.columns:
+        df = df.drop(['gaze_path'], axis=1)
+    if 'folder_num' in  df.columns:
+        df = df.drop(['folder_num'], axis=1)
     return torch.tensor(df.values, dtype=torch.float32)
 
-def load_image_embeddings(data_folder, path):
-    path_to_emb = os.path.join(data_folder, path, "gru", 'images_encoder.pt')
-    emb = torch.load(path_to_emb, map_location=torch.device('cpu'))
-    return emb
-
 class ReadingDataset(Dataset):
-    def __init__(self, grouped_data, sequence_length, data_type, data_folder):
-        """
-        A dataset that handles both standard features and additional embeddings.
-
-        Args:
-            grouped_data (dict): Dictionary containing data groups.
-            sequence_length (int): Sequence length for time-series processing.
-            embeddings (list, optional): List of embeddings corresponding to each group.
-        """
+    def __init__(self, grouped_data, sequence_length, data_type):
         self.sequences = []
         self.labels = []
         self.embeddings = [] if data_type == "emb" else None
-
         for i, (prefix, data) in enumerate(grouped_data.items()):
-            path = os.path.join(data_folder, str(int(data['case'].iloc[0])), data['gaze_path'].iloc[0])
-
             data = data.dropna().reset_index(drop=True)
             if data.empty:
-                print(f"Skipping empty dataset for prefix: {prefix} ; Path: {path}")
+                print(f"Skipping empty dataset for prefix: {prefix}")
                 continue
             y_main = torch.tensor(data['label'].values, dtype=torch.float32)
-            x_main = prepare_features(data)
+            emb_cols = [col for col in data.columns if col.startswith('emb_')]
+            x_embed = torch.tensor(data[emb_cols].values, dtype=torch.float32)
+            x_main = prepare_features(data.drop(columns=emb_cols))
 
-            if data_type == "emb":
-                current_emb = load_image_embeddings(data_folder, path)
-                try:
-                    seq = torch.cat((x_main, current_emb), dim=1)
-                except RuntimeError:
-                    print(len(current_emb), len(x_main), path)
-            else:
-                seq = x_main  # No embeddings, use only prepared features
+            x_main = return_reading_seq(x_main, sequence_length)
+            x_embed = return_reading_seq(x_embed, sequence_length) if data_type == "emb" else None
 
-            # Process sequences
-            seq = return_reading_seq(seq, sequence_length)
-
-            # If embeddings exist, split them after sequence processing
-            if data_type == "emb":
-                x_main_seq, current_emb_seq = torch.split(seq, [x_main.shape[1], current_emb.shape[1]], dim=1)
-                self.embeddings.extend(current_emb_seq.unsqueeze(0))
-                self.sequences.extend(x_main_seq.unsqueeze(0))
-            else:
-                self.sequences.extend(seq.unsqueeze(0))
+            self.sequences.extend(x_main.unsqueeze(0))
             self.labels.extend(y_main[0].unsqueeze(0))
+
+            if data_type == "emb":
+                self.embeddings.extend(x_embed.unsqueeze(0))
 
     def __len__(self):
         return len(self.labels)
@@ -167,14 +141,12 @@ class ReadingDataset(Dataset):
             return self.sequences[idx], self.embeddings[idx], self.labels[idx]
         return self.sequences[idx], self.labels[idx]
 
-
 def get_train_val_test(**kwargs):
     """Loads or prepares train, validation, and test datasets and 
     returns corresponding DataLoader objects.
 
     Parameters:
         kwargs (dict): Optional keyword arguments.
-            - data_folder (str): Path to the folder containing the data.
             - seq_length (int): Sequence length for data processing (default: 240).
             - type (str): Type of data to be processed, e.g., "tabular" (default: "tabular").
             - batch_size (int): Batch size for the DataLoader (default: 64).
@@ -183,34 +155,26 @@ def get_train_val_test(**kwargs):
         tuple: (train_loader, val_loader, test_loader) where each is a 
         DataLoader object.
     """
-    data_folder = kwargs.get("data_folder", "")
     sequence_length = kwargs.get("seq_length", 240)
     data_type = kwargs.get("type", "tabular")
     batch_size = kwargs.get("batch_size", 64)
 
-    train_path = os.path.join(data_folder, "grouped_train.pkl")
-    val_path = os.path.join(data_folder, "grouped_val.pkl")
-    test_path = os.path.join(data_folder, "grouped_test.pkl")
+    train_path = os.path.join(PATH_TO_SAVEDATA, "grouped_elsevier25_train.pkl")
+    val_path = os.path.join(PATH_TO_SAVEDATA, "grouped_elsevier25_val.pkl")
+    test_path = os.path.join(PATH_TO_SAVEDATA, "grouped_elsevier25_test.pkl")
     if os.path.exists(train_path) and os.path.exists(val_path) and os.path.exists(test_path):
         train = load_pickle_file(train_path)
         val = load_pickle_file(val_path)
         test = load_pickle_file(test_path)
     else:
-        train, test, val = prepare_longitudinal_dataset(data_folder)
+        train, test, val = prepare_longitudinal_dataset()
     
-    train_dataset = ReadingDataset(train, sequence_length, data_type, data_folder)
-    test_dataset = ReadingDataset(test, sequence_length, data_type, data_folder)
-    val_dataset = ReadingDataset(val, sequence_length, data_type, data_folder)
+    train_dataset = ReadingDataset(train, sequence_length, data_type)
+    test_dataset = ReadingDataset(test, sequence_length, data_type)
+    val_dataset = ReadingDataset(val, sequence_length, data_type)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=3)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=3)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=3)
 
     return train_loader, val_loader, test_loader
-    
-    
-
-    
-
-
-
